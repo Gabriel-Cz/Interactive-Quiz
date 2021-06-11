030c4cfae63547f6416ebd260033c9b81f990bb4
Gabriel Cazares <gabrielczhz@gmail.com>
2021-06-11 15:03:31 -0500

    done

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="modal">
-           <div >
+           <div class="svg-Wrapper">
                <svg v-if="isCorrect" id="Layer_1" enable-background="new 0 0 512.063 512.063"  viewBox="0 0 512.063 512.063" width="50%" xmlns="http://www.w3.org/2000/svg"><g><g><ellipse cx="256.032" cy="256.032" fill="#00df76" rx="255.949" ry="256.032"/></g><path d="m256.032 0c-.116 0-.231.004-.347.004v512.055c.116 0 .231.004.347.004 141.357 0 255.949-114.629 255.949-256.032s-114.592-256.031-255.949-256.031z" fill="#00ab5e"/><path d="m111.326 261.118 103.524 103.524c4.515 4.515 11.836 4.515 16.351 0l169.957-169.957c4.515-4.515 4.515-11.836 0-16.351l-30.935-30.935c-4.515-4.515-11.836-4.515-16.351 0l-123.617 123.615c-4.515 4.515-11.836 4.515-16.351 0l-55.397-55.397c-4.426-4.426-11.571-4.526-16.119-.226l-30.83 29.149c-4.732 4.475-4.837 11.973-.232 16.578z" fill="#fff5f5"/><path d="m370.223 147.398c-4.515-4.515-11.836-4.515-16.351 0l-98.187 98.187v94.573l145.473-145.473c4.515-4.515 4.515-11.836 0-16.352z" fill="#dfebf1"/></g></svg>
                <svg v-else version="1.1" id="Capa_1" width="50%" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                     viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
@@ -13,8 +13,9 @@
                     383.2,171.099 "/>
                    <g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
                 </svg>
+                <button class="mobileButton" @click="closeModelAndAdvance(false, getNextNumber)">Next Question</button>
            </div>
-           <button @click="closeModelAndAdvance(false, getNextNumber)">Next Question</button>
+           <button class="normalButton" @click="closeModelAndAdvance(false, getNextNumber)">Next Question</button>
        </div>
     </div>
 </template>
@@ -53,10 +54,8 @@ $bgButtonHover: rgba($color: rgb(55, 123, 224), $alpha: 1);
         top: 0;
         left: 0;
         transform: none;
-        width: 100vw;
-        height: 100vh;
-        overflow-y: hidden;
-        padding-right: 15px;
+        width: 100%;
+        height: 100%;
     }
     position: absolute;
     top: 50%;
@@ -71,8 +70,23 @@ $bgButtonHover: rgba($color: rgb(55, 123, 224), $alpha: 1);
     border-radius: 10px;
     display: grid;
     place-items: center;
-    button {
+    .svg-Wrapper {
+        display: grid;
+        place-items: center;
+    }
+    .mobileButton {
         @include mixins.customButton($bgButton, $bgButtonHover);
+        @include mixins.media(xs) {
+            display: flex;
+            margin-top: 50px;
+        }
+      display: none;
+    }
+    .normalButton {
+        @include mixins.customButton($bgButton, $bgButtonHover);
+        @include mixins.media(xs) {
+            display: none;
+        }
     }
 }
 
